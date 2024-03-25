afb3dc40577b67e82d85d51ea3d31e68e9828939
SangJae Lee <dev.codemaestro@gmail.com>
2024-03-25 17:07:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let num = (head/2) + 1
+    let result = []
+    for(let i = num ; i <= head; i++){
+        result
+        
+}
+
+};
