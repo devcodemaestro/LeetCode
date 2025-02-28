c38dd9b35240711d79e493064269bc3b6d1efaf3
SangJae Lee <dev.codemaestro@gmail.com>
2025-02-28 23:49:20 +0900

    Time: 2 ms (46.63%), Space: 55.3 MB (7.05%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -4,11 +4,11 @@
  */
 var longestCommonPrefix = function(strs) {
     let answer = ""
-    let arr = strs[0].split("")
-    for(let i = 0; i < arr.length; i++){
-        let str = arr.slice(0,i+1).join("")
-        if(strs.filter((e)=>e.includes(str)).length === strs.length){
-            answer = str
+    let str = strs[0].split("")
+    for(let i = 0; i < str.length; i++){
+        let result = str.slice(0,i+1).join("")
+        if(strs.filter((e)=>e.indexOf(result) === 0).length === strs.length){
+            answer = result
         }
     }
     return answer
